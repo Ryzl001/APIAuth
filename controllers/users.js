818e27a1f54a15aae17f435308ff14da20cb4137
Stan <potomak123@googlemail.com>
2019-04-24 12:41:18 +0200

    MongoDB

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,29 @@
+const User = require("../models/User");
+
 module.exports = {
   signUp: async (req, res, next) => {
     // try {} catch(error) {} - nie musimy tego używać jak używamy express-promise-router
-    console.log(req.value.body);
     console.log("SignUp called");
+    const { email, password } = req.value.body;
+    // zamiast const email = req.value.body.email
+
+    // Check if there is a user with the same email
+    const foundUser = await User.findOne({ email });
+    if (foundUser) {
+      // dzieki return reszta kodu nei zostanie wykonana, tylko wyskoczy error
+      return res.status(403).json({ error: "Email is already in use" });
+    }
+
+    //create new user
+    const newUser = new User({
+      // zamiast email: email
+      email,
+      password
+    });
+    await newUser.save();
+
+    // response with token
+    res.json({ user: "created" });
   },
   signIn: async (req, res, next) => {
     console.log("SignIn called");
